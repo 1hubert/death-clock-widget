387f06695caf0c5480570057a1821e641f03d4cf
Hubert <hrozmarynowski776@gmail.com>
2021-10-31 19:22:39 +0100

    Change path variable names to guide the user

--- ScheduleTask.bat
+++ ScheduleTask.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET var1="C:\Users\Hubert\Documents\Projekty Python\2020.04\1 - days to DEATH counter in a pseudo-widget format\python.exe"
-SET var2="C:\Users\Hubert\Documents\Projekty Python\2020.04\1 - days to DEATH counter in a pseudo-widget format\main.pyw"
+SET var1="FULL_PATH_TO_PYTHON_EXE_HERE"
+SET var2="FULL_PATH_TO_DEATH_CLOCK.PYW_HERE"
 schtasks /Create /SC DAILY /TN DeathClock /TR %var1=%~1% %var2=%~1%
 PAUSE
