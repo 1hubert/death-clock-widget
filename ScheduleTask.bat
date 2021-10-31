72aa193966c5ce64d66608f7f59b69d3a69a8dfa
Hubert <hrozmarynowski776@gmail.com>
2021-10-31 23:06:42 +0100

    fix: Specify pythonw and correct argument notation

--- ScheduleTask.bat
+++ ScheduleTask.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET var1="FULL_PATH_TO_PYTHON_EXE_HERE"
+SET var1="FULL_PATH_TO_PYTHONW_EXE_HERE"
 SET var2="FULL_PATH_TO_DEATH_CLOCK.PYW_HERE"
-schtasks /Create /SC DAILY /TN DeathClock /TR %var1=%~1% %var2=%~1%
+schtasks /Create /SC DAILY /TN DeathClock /TR "%var1% '%var2%'"
 PAUSE
